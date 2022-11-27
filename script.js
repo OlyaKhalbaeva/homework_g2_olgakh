dfc725cf9fe0c34d3ce0fdc7fc20055b9f624c4a
OlyaKhalbaeva <116864755+OlyaKhalbaeva@users.noreply.github.com>
2022-11-27 16:47:07 +0800

    hw7 start

---
+++ script.js
@@ -0,0 +1,8 @@
+const MyName = "Ольга";
+const MyCity = "Улан-Удэ";
+const MyJob = "работаю в сфере ВЭД";
+const MyDateOfBirth = new Date(1992,4,11);
+let now = new date ();
+let today= new date (now.getFullYear);
+let strStore= `Привет! Меня зовут ${MyName}. Мне ${now.getFullYear() - birthday.getFullYear()}. Сейчас я ${MyJob} в городе ${MyCity}`;
+console.log(strStore);

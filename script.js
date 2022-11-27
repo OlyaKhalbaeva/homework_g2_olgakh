5056361a5428d9b99d90e228545dbe2f8212bca2
OlyaKhalbaeva <116864755+OlyaKhalbaeva@users.noreply.github.com>
2022-11-27 17:14:20 +0800

    js fix3

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ const MyJob = "работаю в сфере ВЭД";
 const MyDateOfBirth = new Date(1992,4,11);
 let now = new Date();
 let today= new Date (now.getFullYear);
-let strStore= `Привет! Меня зовут ${MyName}. Мне ${now.getFullYear() - birthday.getFullYear()}. Сейчас я ${MyJob} в городе ${MyCity}`;
+let strStore= `Привет! Меня зовут ${MyName}. Мне ${now.getFullYear() - MyDateOfBirth.getFullYear()}. Сейчас я ${MyJob} в городе ${MyCity}`;

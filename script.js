dc208e25a2ba3faf074de1443659c746ce802175
OlyaKhalbaeva <116864755+OlyaKhalbaeva@users.noreply.github.com>
2022-11-27 16:57:46 +0800

    fix

--- script.js
+++ script.js
@@ -5,4 +5,3 @@ const MyDateOfBirth = new Date(1992,4,11);
 let now = new date ();
 let today= new date (now.getFullYear);
 let strStore= `Привет! Меня зовут ${MyName}. Мне ${now.getFullYear() - birthday.getFullYear()}. Сейчас я ${MyJob} в городе ${MyCity}`;
-console.log(strStore);
